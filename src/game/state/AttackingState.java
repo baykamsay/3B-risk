addcfdb04fa162328d2685ad8eda5d32ccd4a7c8
LAPTOP-2NE4OHFB\hp <aleynasutbas@hotmail.com>
2020-12-09 19:34:03 +0300

    correction about the event method

--- src/game/state/AttackingState.java
+++ src/game/state/AttackingState.java
@@ -3,11 +3,10 @@ package game.state;
 import game.GameEngine;
 import game.player.Territory;
 import game.scene.MapScene;
+import javafx.event.Event;
 import javafx.scene.Scene;
 import menu.GameMenuManager;
 import menu.MenuState;
-
-import java.util.ArrayList;
 
 public class AttackingState implements MenuState {
 
@@ -21,27 +20,28 @@ public class AttackingState implements MenuState {
     public int attackingArmies;
     public int defendingArmies;
     public AttackingArmySelectionMenuState armySelectionState;
-    public AttackingPlanningState planningState;
+    public AttackingPlanningState AttackPlanningState;
     public WarState warState;
 
     public AttackingState(int width, int height, GameEngine engine) {
         this.width = width;
         this.height = height;
         this.engine = engine;
-        init();
     }
 
-    public void init(){
-        //plan attacking
-        planningState = new AttackingPlanningState(width,height,engine);
-        destination = planningState.getDestination();
-        source = planningState.getSource();
-        //select armies
-        armySelectionState = new ArmySelectionState(width,height,engine);
-        attackingArmies = armySelectionState.getAttackingArmies();
-        defendingArmies = armySelectionState.getDefendingArmies();
-        //war
-        warState = new WarState(width,height,engine);
+    public void Attack(Event e){
+        if((e.getSource().toString().equals("PASS"))){
+            //plan attacking
+            AttackPlanningState = new AttackingPlanningState(width, height, engine);
+            destination = AttackPlanningState.getDestination();
+            source = AttackPlanningState.getSource();
+            //select armies
+            armySelectionState = new ArmySelectionState(width, height, engine);
+            attackingArmies = armySelectionState.getAttackingArmies();
+            defendingArmies = armySelectionState.getDefendingArmies();
+            //war - not complete
+            warState = new WarState(width, height, engine); //pass armies as parameters?
+        }
     }
 
     @Override
