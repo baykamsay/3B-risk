60e4296e94b52e3156c5f7e625bf879621dc8431
Baykam Say <baykam.say@gmail.com>
2020-12-11 11:38:03 +0300

    Create WarState.java

---
+++ src/game/state/WarState.java
@@ -0,0 +1,11 @@
+package game.state;
+
+import javafx.event.ActionEvent;
+
+public class WarState implements GameState{
+
+    @Override
+    public void mapSelect(ActionEvent e) {
+        // to do
+    }
+}
