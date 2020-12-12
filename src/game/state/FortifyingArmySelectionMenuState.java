fd9ad4045f54a6997fdfa859ba5a1c2c232065c1
LAPTOP-UH0D04OH\MELİH <diksumelih@gmail.com>
2020-12-12 21:51:57 +0300

    Implemented FortifyingArmySelectionMenuState

---
+++ src/game/state/FortifyingArmySelectionMenuState.java
@@ -0,0 +1,46 @@
+package game.state;
+
+import game.GameEngine;
+import javafx.event.ActionEvent;
+
+public class FortifyingArmySelectionMenuState implements GameState{
+    private static FortifyingArmySelectionMenuState instance;
+    private FortifyingState fortify;
+    private GameEngine engine;
+
+    private FortifyingArmySelectionMenuState() {
+        fortify = FortifyingState.getInstance();
+        engine = GameEngine.getInstance();
+    }
+
+    public static FortifyingArmySelectionMenuState getInstance() {
+        if (instance == null) {
+            synchronized (FortifyingArmySelectionMenuState.class) {
+                if (instance == null) {
+                    instance = new FortifyingArmySelectionMenuState();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void displayArmyNumberSelection(ActionEvent e) {
+        //to do
+        //setNumOfArmies(...);
+        engine.setPlayerTurn(engine.getPlayerTurn() + 1);
+        engine.switchState(ArmyPlacementState.getInstance());
+    }
+
+    public void setNumOfArmies(int armies){
+        fortify.getDestination().setNumOfArmies(armies);
+        fortify.getSource().setNumOfArmies(fortify.getSource().getNumOfArmies() - armies);
+    }
+
+    public void mapSelect(ActionEvent e) {
+        if (fortify.getSource().getNumOfArmies() > 1) {
+            displayArmyNumberSelection(e);
+        }
+    }
+
+
+}
