c818bcb0e7fc5723d2e29ad7a8cc5688ea0a1c2c
Baykam Say <baykam.say@gmail.com>
2020-12-16 23:07:32 +0300

    Update ArmyPlacementState.java

--- src/game/state/ArmyPlacementState.java
+++ src/game/state/ArmyPlacementState.java
@@ -3,24 +3,16 @@ package game.state;
 import game.GameEngine;
 import game.player.Player;
 import game.player.Territory;
-import game.scene.MapScene;
 import javafx.event.ActionEvent;
-import javafx.scene.Scene;
-import menu.GameMenuManager;
-import menu.MenuState;
-
-import java.util.ArrayList;
 
 public class ArmyPlacementState implements GameState {
     private static ArmyPlacementState instance;
     private GameEngine engine;
-    private int currentPlayer, addibleArmyNo, chosenArmyNo;
+    private int addibleArmyNo;
 
     private ArmyPlacementState() {
         engine = GameEngine.getInstance();
-        currentPlayer = 0;
-        addibleArmyNo = 0;
-        chosenArmyNo = 0;
+        calculateNumberOfArmies(engine.getCurrentPlayer());
     }
 
     public static ArmyPlacementState getInstance() {
@@ -38,7 +30,7 @@ public class ArmyPlacementState implements GameState {
     public void mapSelect(ActionEvent e) {
         Territory[] territories = engine.getMap().getTerritories();
         if (addibleArmyNo != 0) {
-            for (Territory territory : territories) {
+            for (Territory territory : territories) { // disabling the button is a better solution
                 if ((e.getSource() == territory) && (engine.getCurrentPlayer() == territory.getRuler())) {
                     territory.setNumOfArmies(territory.getNumOfArmies() + 1);
                     addibleArmyNo = addibleArmyNo - 1;
@@ -64,13 +56,5 @@ public class ArmyPlacementState implements GameState {
 
     public int getAddibleArmyNo(){
         return addibleArmyNo;
-    }
-
-    public int getChosenArmyNo(){
-        return chosenArmyNo;
-    }
-
-    public void setChosenArmyNo(int armyNo){
-        chosenArmyNo = armyNo;
     }
 }
