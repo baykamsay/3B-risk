828ff0de22c51ad8e35ed3d25913fda47a70a1d8
Baykam Say <baykam.say@gmail.com>
2020-12-17 23:56:55 +0300

    Fix GameMap

--- src/game/player/Area.java
+++ src/game/player/Area.java
@@ -1,25 +1,16 @@
 package game.player;
 
 import java.util.ArrayList;
-/*
-public enum  AreaEnum {
-    BILKENTISLAND, EASTCAMPUS, LOWERMAINCAMPUS, UPPERMAINCAMPUS;
 
-}*/
-public class Area{
+public class Area implements Place{
     private ArrayList<Territory> territories;
     private String name;
 
-    //This constructor requires to add territories later
     public Area(String name){
         territories = new ArrayList<>();
         this.name = name;
     }
 
-    public Area(String name, ArrayList<Territory> t){
-        this.name = name;
-        this.territories = t;
-    }
     public String getName(){
         return name;
     }
