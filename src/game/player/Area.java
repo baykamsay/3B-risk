3f6c3226c7cd0b86cf6c6bf863cb5cc7d9850268
yerkal <yerkal1998@gmail.com>
2020-12-17 14:59:13 +0300

    Area is converted from enum to class

--- src/game/player/Area.java
+++ src/game/player/Area.java
@@ -1,5 +1,32 @@
 package game.player;
 
-public enum Area {
-    BILKENTISLAND, EASTCAMPUS, LOWERMAINCAMPUS, UPPERMAINCAMPUS
+import java.util.ArrayList;
+/*
+public enum  AreaEnum {
+    BILKENTISLAND, EASTCAMPUS, LOWERMAINCAMPUS, UPPERMAINCAMPUS;
+
+}*/
+public class Area{
+    private ArrayList<Territory> territories;
+    private String name;
+
+    //This constructor requires to add territories later
+    Area(String name){
+        territories = new ArrayList<>();
+        this.name = name;
+    }
+    //This constructor initializes the territories
+    Area(String name, ArrayList<Territory> t){
+        this.name = name;
+        this.territories.addAll(t);
+    }
+    public String getName(){
+        return name;
+    }
+    public void addTerritory(Territory t){
+        territories.add(t);
+    }
+    public void cloneTerritories(ArrayList<Territory> t){
+        territories.addAll(t);
+    }
 }
