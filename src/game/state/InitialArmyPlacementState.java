46a0cfbfc1609ae86946b0a2395db3f7ccbb782f
LAPTOP-2NE4OHFB\hp <aleynasutbas@hotmail.com>
2020-12-19 15:57:29 +0300

    adjustments about the army placement

--- src/game/state/InitialArmyPlacementState.java
+++ src/game/state/InitialArmyPlacementState.java
@@ -11,10 +11,10 @@ public class InitialArmyPlacementState implements GameState {
     private static InitialArmyPlacementState instance;
     private GameEngine engine;
     private int currentPlayer;
+    private ArrayList<Integer> armyCounts;
 
     private InitialArmyPlacementState() {
         engine = GameEngine.getInstance();
-        currentPlayer = 0;
     }
 
     public static InitialArmyPlacementState getInstance() {
@@ -32,29 +32,78 @@ public class InitialArmyPlacementState implements GameState {
     public void mapSelect(int territory) {
         Territory t = engine.getMap().getTerritory(territory);
         ArrayList<Player> players = engine.getPlayers();
-            // getName() can also be getId() if ids are implemented and the other part requires something to compare
-            // when the map is implemented fix this
-            // && territory.getRuler() == null this can also be checked but if it is not null disabling the button is
-            // better
+        if(!territoryCheck()) {
+            //if there are territories left, keep selecting
             if (t.getRuler() == null) {
                 t.setRuler(players.get(currentPlayer));
+                t.setNumOfArmies(t.getNumOfArmies() + 1);
+                armyCounts.set(currentPlayer,armyCounts.get(currentPlayer)); //-1 from total army counts
+                currentPlayer = (currentPlayer + 1) % engine.getPlayers().size();
             }
+        }
+        else {
+            if (t.isRuler(engine.getPlayers().get(currentPlayer))){
+                t.setNumOfArmies(t.getNumOfArmies() + 1);
+                armyCounts.set(currentPlayer,armyCounts.get(currentPlayer)); //-1 from total army counts
+                currentPlayer = (currentPlayer + 1) % engine.getPlayers().size();
+            }
+        }
         checkIfStateOver();
-        currentPlayer++;
+    }
+
+    //initialize army counts for players
+    public void calculateArmyCounts(){
+        if(engine.getPlayers().size() == 2){
+            armyCounts.add(40);
+            armyCounts.add(40);
+        }
+        else if(engine.getPlayers().size() == 3){
+            for(int i = 0; i < 3; i++){
+                armyCounts.add(35);
+            }
+        }
+        else if(engine.getPlayers().size() == 4){
+            for(int i = 0; i < 4; i++){
+                armyCounts.add(30);
+            }
+        }
+        else if(engine.getPlayers().size() == 5){
+            for(int i = 0; i < 5; i++){
+                armyCounts.add(25);
+            }
+        }
+        else if(engine.getPlayers().size() == 6){
+            for(int i = 0; i < 6; i++){
+                armyCounts.add(20);
+            }
+        }
     }
 
     @Override
     public void start() {
         engine.mapScene.getController().setState(0);
         currentPlayer = 0;
+        calculateArmyCounts();
     }
+
+    //check if all of the territories are empty
+    public boolean territoryCheck(){
+        Territory[] territories = engine.getMap().getTerritories();
+        boolean territoryOver = true;
+        for (Territory territory : territories) {
+            if (territory.getRuler() == null) {
+                territoryOver = false;
+            }
+        }
+        return territoryOver;
+    }
+
 
     // Is the state really over when no territory left empty?
     public void checkIfStateOver() {
-        Territory[] territories = engine.getMap().getTerritories();
         boolean stateOver = true;
-        for (Territory territory : territories) {
-            if (territory.getRuler() == null) {
+        for (int i: armyCounts) {
+            if (i != 0) {
                 stateOver = false;
             }
         }
